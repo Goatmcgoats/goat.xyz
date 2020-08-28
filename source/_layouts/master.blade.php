e48288c0597a01b690ce8fe0c1b66b09cde3c0d0
Kyle <Goatmcgoats@users.noreply.github.com>
2020-08-28 11:47:15 -0400

    Update master.blade.php

--- source/_layouts/master.blade.php
+++ source/_layouts/master.blade.php
@@ -2,7 +2,7 @@
 <html lang="{{ $page->language ?? 'en' }}">
 <head>
     <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="x-ua-compatible" content="ie=edge">
     <title>
         @yield('title')
