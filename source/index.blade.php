4038eb4c742e84a6f214208909acb8ba97d8b7a0
Kyle <Goatmcgoats@users.noreply.github.com>
2020-08-28 11:52:03 -0400

    Update index.blade.php

--- source/index.blade.php
+++ source/index.blade.php
@@ -1,6 +1,23 @@
 @extends('_layouts.master')
 
 @section('content')
+    <style>
+* {
+  box-sizing: border-box;
+}
+
+.row {
+  display: flex;
+}
+
+/* Create two equal columns that sits next to each other */
+.column {
+  flex: 50%;
+  padding: 10px;
+  height: 300px; /* Should be removed. Only for demonstration */
+}
+</style>
+
     <h1>Welcome to Artisan Static</h1>
 
     <p>Artisan Static is a starter template for building a static Jigsaw blog hosted on Netlify.</p>
@@ -14,4 +31,12 @@
     <p>CSS test: <span class="test-css">text with blue border</span></p>
 
     <p>JavaScript test: <a href="#test" class="test-js">click me</a></p>
+    
+    <div class="row">
+    <div class="column"></div>
+    <div class="column"></div>
+    </div>
+    
+    
+    
 @endsection
